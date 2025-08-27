e555812e158fecd7cda224aceb074e29e2e2c42a
Ryanchen5243 <rchen5243@gmail.com>
2025-08-27 15:30:43 -0400

    first request to places api - nearby search

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,4 +1,38 @@
-def data_parse():
-    print("hello")
+import sys
+from dotenv import load_dotenv
+import os
+import requests
+import json
+def g_maps_lambda():
+    load_dotenv()
+    API_KEY = os.getenv('G_MAPS_API_KEY')
+    url = "https://places.googleapis.com/v1/places:searchNearby"
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': API_KEY,
+        'X-Goog-FieldMask': 'places.displayName'
+    }
+    body = {
+        'includedTypes': [
+            'cafe',
+        ],
+        'maxResultCount': 10,
+        'locationRestriction': {
+            'circle': {
+                'center': {
+                    'latitude': 40.7200,
+                    'longitude': -73.999,
+                },
+                'radius': 500.0,
+            }
+        }
+    }
+    response = requests.post(url, headers=headers, json=body)
+    if response.status_code == 200:
+        print("POST Request Successful")
+        res_dict = response.json()
+        print(json.dumps(res_dict))
+    else:
+        print("Error:", response.status_code)
 if __name__ == "__main__":
-    data_parse()
+    g_maps_lambda()
